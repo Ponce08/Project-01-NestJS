29ad45f3304962c66c4675a9583caa5754b1c613
ponce08 <yonathanponce2019@gmail.com>
2025-06-26 11:59:33 -0500

    Project01-nesJS-08

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { ProjectsController } from './projects/projects.controller';
-import { Projects,Controller } from './projects,/projects,.controller';
 
 @Module({
-    controllers:[UsersController, ProjectsController, Projects,Controller]
+    controllers:[UsersController]
 })
 export class UsersModule {}
