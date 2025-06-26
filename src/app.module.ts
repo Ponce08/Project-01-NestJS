92bee3c6853e2a8197a6709b6baedd9a64110e7f
ponce08 <yonathanponce2019@gmail.com>
2025-06-26 12:40:48 -0500

    Project01-nesJS-05

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from 'src/tasks/tasks.module.js';
+import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, ProjectsModule, AuthModule],
 })
 export class AppModule {}
