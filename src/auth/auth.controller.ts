5e09a40bc5c4f160e01a4d9b3db9020e78aff10e
ponce08 <yonathanponce2019@gmail.com>
2025-07-02 16:27:26 -0500

    Project01-nesJS-18.3

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,3 @@ import { Controller } from '@nestjs/common';
 @Controller('auth')
 export class AuthController {}
 
-// import { Controller } from '@nestjs/common';
-
-// @Controller('auth')
-// export class AuthController {}
