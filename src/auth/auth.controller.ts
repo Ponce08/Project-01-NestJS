42f4e0496e91d7a84583da7f6898fe1caa90f9ef
ponce08 <yonathanponce2019@gmail.com>
2025-07-10 08:08:22 -0500

    project-nestjs-28

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,3 +3,7 @@ import { Controller } from '@nestjs/common';
 @Controller('auth')
 export class AuthController {}
 
+// import { Controller } from '@nestjs/common';
+
+// @Controller('auth')
+// export class AuthController {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,3 +7,13 @@ import { AuthService } from './auth.service';
   providers: [AuthService]
 })
 export class AuthModule {}
+
+// import { Module } from '@nestjs/common';
+// import { AuthController } from './auth.controller';
+// import { AuthService } from './auth.service';
+
+// @Module({
+//   controllers: [AuthController],
+//   providers: [AuthService]
+// })
+// export class AuthModule {}
