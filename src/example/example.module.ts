31b17bfa0246f95f034da3efd39ece87634fc4bc
ponce08 <yonathanponce2019@gmail.com>
2025-07-10 08:06:26 -0500

    project-nestjs-27

--- src/example/example.module.ts
+++ src/example/example.module.ts
@@ -5,3 +5,12 @@ import { ExampleService } from './example.service';
   providers: [ExampleService]
 })
 export class ExampleModule {}
+
+
+// import { Module } from '@nestjs/common';
+// import { ExampleService } from './example.service';
+
+// @Module({
+//   providers: [ExampleService]
+// })
+// export class ExampleModule {}
--- src/example/example.service.ts
+++ src/example/example.service.ts
@@ -2,3 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ExampleService {}
+
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class ExampleService {}
