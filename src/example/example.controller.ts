93f3d50b70abe0d6ec14860d33a9d350002fb776
ponce08 <yonathanponce2019@gmail.com>
2025-07-10 07:36:09 -0500

    project-nestjs-24

--- src/example/example.controller.ts
+++ src/example/example.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('example')
-export class ExampleController {}
+export class ExampleController {
+  @Get('/example')
+  index(@Req() req: Request, @Res() res: Response) {
+    res.status(200).json({ message: 'Hello World' });
+  }
+}
