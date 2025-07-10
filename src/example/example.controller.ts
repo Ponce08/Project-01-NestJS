81981fe1b19c15ba42762a7c0e0e71ab8512fe47
ponce08 <yonathanponce2019@gmail.com>
2025-07-10 07:47:06 -0500

    project-nestjs-25

--- src/example/example.controller.ts
+++ src/example/example.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller('example')
 export class ExampleController {
-  @Get('/example')
+  @Get()
   index(@Req() req: Request, @Res() res: Response) {
     res.status(200).json({ message: 'Hello World' });
+  }
+
+  @Get('/notfound')
+  @HttpCode(404)
+  notFound() {
+    return { message: 'Page not found' };
+  }
+
+  @Get('/error')
+  @HttpCode(500)
+  errorPge() {
+    return { message: 'error in the server' };
   }
 }
