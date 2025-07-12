a77b861dd310c97a92aabcaee932740bb6008aeb
ponce08 <yonathanponce2019@gmail.com>
2025-07-11 19:54:13 -0500

    project-nestjs-31

--- src/example/pipes/validate-example/validate-example.pipe.ts
+++ src/example/pipes/validate-example/validate-example.pipe.ts
@@ -1,10 +1,18 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
 
 @Injectable()
 export class ValidateExamplePipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    console.log('Value', value);
-
-    return value;
+    const ageNumber = parseInt(value.age.toString(), 10);
+    if (isNaN(ageNumber)) {
+      throw new HttpException('Age must be number', HttpStatus.BAD_REQUEST);
+    }
+    return { ...value, age: ageNumber };
   }
 }
